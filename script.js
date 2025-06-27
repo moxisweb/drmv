51660aa183c77f85343555f12af723e94eebd8f0
moxis-web <claus.goyarrola@outlook.com>
2025-06-27 18:00:02 +0200

    Added imgs; started edge data

--- script.js
+++ script.js
@@ -15,92 +15,12 @@ var cy = cytoscape({
   layout: {
     name: 'cose'
   },
-  minZoom: 2,
-  maxZoom: 8,
+  //minZoom: 2,
+  //maxZoom: 8,
   wheelSensitivity: 1.2,
   autoungrabify: true,
   style: dataArray[0],
   elements: dataArray[1]
-/*   [
-    {
-      selector: 'node',
-      style: {
-        'shape': 'rectangle',
-        'height': '15px',
-        'width': '15px',
-        'background-image': 'data(img)',
-        'background-fit': 'cover',
-        'background-color': '#000',
-        'outline-width': 0.8,
-        'outline-color': 'purple',
-        'label': 'data(label)',
-        'font-size': 4,
-        'text-justification': 'center',
-        'text-wrap': 'wrap',
-        'text-max-width': '30px',
-        'text-margin-y': '2.5px',
-        'color': '#fff',
-        'text-outline-color': '#000',
-        'text-outline-width': 0.5,
-        'text-valign': 'bottom',
-        'text-halign': 'center'
-      }
-    },
-    {
-      selector: '.main',
-      style: {
-        'font-weight': 'bold'
-      }
-    },
-    {
-      selector: '.none',
-      style: {
-        'opacity': '0.3'
-      }
-    },
-    {
-      selector: '.undertale',
-      style: {
-        'outline-color': 'red'
-      }
-    },
-    {
-      selector: '.other',
-      style: {
-        'outline-color': 'blue'
-      }
-    },
-    {
-      selector: 'edge',
-      style: {
-        'width': 0.35,
-        'line-color': '#fff',
-        'opacity': 0.8,
-        'curve-style': 'unbundled-bezier',
-        'control-point-step-size': 8,
-        'target-arrow-color': '#fff',
-        'target-arrow-shape': 'triangle',
-        'arrow-scale': 0.3,
-        'target-distance-from-node': 2,
-        'target-endpoint': 'outside-to-line-or-label'
-      }
-    },
-    {
-      selector: '.ori2',
-      style: {
-        'width': 0.25,
-        'opacity': '0.5'
-      }
-    },
-    {
-      selector: '.ori3',
-      style: {
-        'width': 0.3,
-        'line-style': 'dotted',
-        'opacity': '0.5'
-      }
-    }
-  ], */
 });
 
 const array1 = cy.nodes();
@@ -140,11 +60,23 @@ fulllayout.run();
 
 //////////////////////////////////////////////////////////////
 
-// Interactive function: tap node to select + open song info
+// Interactive function: tap node to select + open song info (WIP)
 
 cy.on('tap', 'node', function(evt){
   var node = evt.target;
   console.log( 'tapped ' + node.id() );
+});
+
+// Interactive function: tap edge to color shared motif (WIP)
+
+cy.on('tap', 'edge', function(evt){
+  var edge = evt.target;
+  if ("motif" in edge.data()) {
+    var m = edge.data("motif");
+    cy.elements("edge[motif = '" + m + "']").forEach(function(ele) {
+      ele.addClass("highlight");
+    });
+  }
 });
 
 // Menu buttons
