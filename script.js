a3e2eac35fa4ca9c2f75c18523239b134922d39e
moxis-web <claus.goyarrola@outlook.com>
2025-07-25 11:51:02 +0200

    Added cy.ready

--- script.js
+++ script.js
@@ -38,10 +38,13 @@ var cy = cytoscape({
 ///////////////////////////// Auto data changes
 
 //add simplified motif names
-cy.nodes().forEach(function(node) { 
-  var nodeClasses = node.data("classes");
-  var nodeSClasses = nodeClasses.map(function(x){return x.replace( /[\W_\s]+/g, '' )});
-    node.data( "sclasses", nodeSClasses )
+
+cy.ready(function(){
+  cy.nodes().forEach(function(node) { 
+    var nodeClasses = node.data("classes");
+    var nodeSClasses = nodeClasses.map(function(x){return x.replace( /[\W_\s]+/g, '' )});
+      node.data( "sclasses", nodeSClasses )
+  });
 });
 
 ///////////////////////////// Interactive functions
