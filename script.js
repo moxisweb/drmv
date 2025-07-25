763c4a586fbab1348cfe80119a5fb7128a6535af
moxis-web <claus.goyarrola@outlook.com>
2025-07-25 14:16:53 +0200

    Test console with classes as sclasses

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ cy.ready(function(){
   cy.nodes().forEach(function(node) { 
     var nodeClasses = node.classes();
     var nodeSClasses = nodeClasses.map(function(x){return x.replace( /[\W_\s]+/g, '' )});
-    node.data( 'sclasses', 'testvalue' ); //
+    node.data( 'sclasses', nodeClasses ); //
   });
 });
 
