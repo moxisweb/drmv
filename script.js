c58257b8e5457d369e531737b5c6769a46a98c2f
moxis-web <claus.goyarrola@outlook.com>
2026-02-22 23:13:20 +0100

    edge style tweak, more long edges

--- script.js
+++ script.js
@@ -51,7 +51,9 @@ cy.nodes().forEach(function(node) {
 // CREATE LONG EDGES
 cy.add([
   { group: 'edges', data: { source: '133', target: '206', motif: "freedom" }, classes: ["weak"] },
-  { group: 'edges', data: { source: '036', target: '239', motif: "dummy" } }
+  { group: 'edges', data: { source: '036', target: '239', motif: "dummy" } },
+  { group: 'edges', data: { source: '901', target: '230', motif: "lostgirl" }, classes: ["weak"] },
+  { group: 'edges', data: { source: '135', target: '417', motif: "holy" }, classes: ["weak"] }
 ]);
 
 ///////////////////////////// Interactive functions
