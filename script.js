72d236f4f95ab8c8a0c266d361a7c7e3ed6a6aca
moxis-web <claus.goyarrola@outlook.com>
2026-02-28 23:52:43 +0100

    Rollback

--- script.js
+++ script.js
@@ -56,14 +56,14 @@ cy.add([
   { group: 'edges', data: { source: '107', target: '133', motif: "chase" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '137', motif: "dont" } },
   { group: 'edges', data: { source: '135', target: '140', motif: "holy" }, classes: ["weak"] },
+  { group: 'edges', data: { source: '302', target: '221', motif: "hey" } },
   { group: 'edges', data: { source: '139', target: '236', motif: "dont" } },
   { group: 'edges', data: { source: '079', target: '236', motif: "nightmare" }, classes: ["weak"] },
-  { group: 'edges', data: { source: '133', target: '239', motif: "freedom" } },
   { group: 'edges', data: { source: '139', target: '302', motif: "dont"}, classes: ["weak"] },
+  { group: 'edges', data: { source: '302', target: '304', motif: "Tenna" } },
   { group: 'edges', data: { source: '139', target: '307', motif: "dont"}, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '323', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '230', target: '330', motif: "lostgirl" }, classes: ["weak"] },
-  { group: 'edges', data: { source: '135', target: '333', motif: "holy" } },
   { group: 'edges', data: { source: '130', target: '338', motif: "Lancer" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '404', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '104', target: '404', motif: "Susie" }, classes: ["weak"] },
@@ -71,7 +71,8 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '412', motif: "dont" } },
   { group: 'edges', data: { source: '139', target: '422', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '108', target: '422', motif: "legend" } },
-  { group: 'edges', data: { source: '304', target: '438', motif: "Tenna" } },
+  { group: 'edges', data: { source: '302', target: '438', motif: "hey" } }
+
 ]);
 
 ///////////////////////////// Interactive functions
