8a35caf21c54e1f908db2ac8949d7d9cff9a8642
moxis-web <claus.goyarrola@outlook.com>
2026-03-01 00:20:08 +0100

    Ch 3 position tweaks

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ cy.nodes().forEach(function(node) {
 
 // CREATE LONG EDGES
 cy.add([
-  { group: "edges", data: { source: "135", target: "903", motif: "holy" }, classes: ["weak"] },
+  { group: 'edges', data: { source: '135', target: '903', motif: "holy" }, classes: ["weak"] },
   { group: 'edges', data: { source: '101', target: '119', motif: "gaster" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '133', motif: "dont" } },
   { group: 'edges', data: { source: '107', target: '133', motif: "chase" }, classes: ["weak"] },
-  { group: "edges", data: { source: "101", target: "133", motif: "gaster" } },
+  { group: 'edges', data: { source: '101', target: '133', motif: "gaster" } },
   { group: 'edges', data: { source: '139', target: '137', motif: "dont" } },
   { group: 'edges', data: { source: '135', target: '140', motif: "holy" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '236', motif: "dont" } },
@@ -64,7 +64,6 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '307', motif: "dont"}, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '323', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '230', target: '330', motif: "lostgirl" }, classes: ["weak"] },
-  { group: "edges", data: { source: "135", target: "333", motif: "holy" } },
   { group: 'edges', data: { source: '130', target: '338', motif: "Lancer" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '404', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '104', target: '404', motif: "Susie" }, classes: ["weak"] },
@@ -72,7 +71,7 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '412', motif: "dont" } },
   { group: 'edges', data: { source: '139', target: '422', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '108', target: '422', motif: "legend" } },
-  { group: "edges", data: { source: "304", target: "438", motif: "Tenna" } }
+  { group: 'edges', data: { source: '302', target: '438', motif: "hey" } }
 
 ]);
 
