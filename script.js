73de57daabff6afdcc11016de3dc477a04e3d9a2
moxis-web <claus.goyarrola@outlook.com>
2026-03-01 17:39:59 +0100

    Ch 3 tweaks position

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '236', motif: "dont" } },
   { group: 'edges', data: { source: '079', target: '236', motif: "nightmare" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '302', motif: "dont"}, classes: ["weak"] },
-  { group: 'edges', data: { source: '302', target: '304', motif: "Tenna" } },
   { group: 'edges', data: { source: '139', target: '307', motif: "dont"}, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '323', motif: "dont" }, classes: ["weak"] },
   { group: 'edges', data: { source: '230', target: '330', motif: "lostgirl" }, classes: ["weak"] },
@@ -70,9 +69,8 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '411', motif: "dont" } },
   { group: 'edges', data: { source: '139', target: '412', motif: "dont" } },
   { group: 'edges', data: { source: '139', target: '422', motif: "dont" }, classes: ["weak"] },
-  { group: 'edges', data: { source: '108', target: '422', motif: "legend" } },
-  { group: 'edges', data: { source: '302', target: '438', motif: "hey" } }
-
+  { group: 'edges', data: { source: '108', target: '422', motif: "legend" } }
+  
 ]);
 
 ///////////////////////////// Interactive functions
