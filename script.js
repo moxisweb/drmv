e47369a34c8865cd8c9a22cf740556c1cb4df5cf
moxis-web <claus.goyarrola@outlook.com>
2026-03-01 17:54:49 +0100

    Position tweak

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '302', motif: "dont"}, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '307', motif: "dont"}, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '323', motif: "dont" }, classes: ["weak"] },
+  { group: 'edges', data: { source: '135', target: '332', motif: "holy" } },
   { group: 'edges', data: { source: '230', target: '330', motif: "lostgirl" }, classes: ["weak"] },
   { group: 'edges', data: { source: '130', target: '338', motif: "Lancer" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '404', motif: "dont" }, classes: ["weak"] },
