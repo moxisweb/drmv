70eb7f0078935e8e6793e8e2981968bf9fc8e815
moxis-web <claus.goyarrola@outlook.com>
2026-04-29 17:14:23 +0200

    Edits

--- script.js
+++ script.js
@@ -59,7 +59,6 @@ cy.add([
   { group: 'edges', data: { source: '139', target: '133', motif: "dont" } },
   { group: 'edges', data: { source: '107', target: '133', motif: "chase" }, classes: ["weak"] },
   { group: 'edges', data: { source: '139', target: '137', motif: "dont" } },
-  { group: 'edges', data: { source: '135', target: '140', motif: "holy" }, classes: ["weak"] },
   { group: 'edges', data: { source: '133', target: '206', motif: "freedom" }, classes: ["weak"] },
   { group: 'edges', data: { source: '302', target: '221', motif: "hey" } },
   { group: 'edges', data: { source: '336', target: '222', motif: "nightmare" }, classes: ["weak"] },
