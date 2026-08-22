b869ff6d7b1314c92a455b9bd42483f40961412d
moxisweb <claus.goyarrola@outlook.com>
2026-08-22 18:30:06 +0200

    multiple selector format change

--- script.js
+++ script.js
@@ -376,29 +376,27 @@ chapterbtn.addEventListener("click", function() {
   cy.edges("edge[target < '100' ]").forEach(function(edge) {
     edge.addClass('edgeUndertale');
   });
-  cy.edges("edge[target > '199' && target < '300' ]").forEach(function(edge) {
-    edge.addClass('edgeCh2');
-  });
-  /*
-  cy.edges("edge[target > '99' && target < '200' ]").forEach(function(edge) {
+  cy.edges("edge[target > '99'][target < '200']").forEach(function(edge) {
     edge.addClass('edgeCh1');
   });
-  cy.edges("edge[target > '299' && target < '400' ]").forEach(function(edge) {
+  cy.edges("edge[target > '199'][target < '300']").forEach(function(edge) {
+    edge.addClass('edgeCh2');
+  });
+  cy.edges("edge[target > '299'][target < '400']").forEach(function(edge) {
     edge.addClass('edgeCh3');
   });
-  cy.edges("edge[target > '399' && target < '500' ]").forEach(function(edge) {
+  cy.edges("edge[target > '399'][target < '500']").forEach(function(edge) {
     edge.addClass('edgeCh4');
   });
-  cy.edges("edge[target > '499' && target < '600' ]").forEach(function(edge) {
+  cy.edges("edge[target > '499'][target < '600']").forEach(function(edge) {
     edge.addClass('edgeCh5');
   });
-  cy.edges("edge[target > '799' && target < '900' ]").forEach(function(edge) {
+  cy.edges("edge[target > '799'][target < '900']").forEach(function(edge) {
     edge.addClass('edgeUnlisted');
   });
-  cy.edges("edge[target > '899' ]").forEach(function(edge) {
+  cy.edges("edge[target > '899']").forEach(function(edge) {
     edge.addClass('edgeOther');
   });
-  */
 });
 
 
